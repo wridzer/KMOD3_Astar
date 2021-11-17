0f0f8a7d40138470ef8e57cf841756912b035582
Wridzer Kamphuis <wridzer12@gmail.com>
2021-11-17 22:53:30 +0100

    working on it

--- Astar/Assets/Scripts/Astar.cs
+++ Astar/Assets/Scripts/Astar.cs
@@ -14,9 +14,73 @@ public class Astar
     /// <param name="endPos"></param>
     /// <param name="grid"></param>
     /// <returns></returns>
+    /// 
+    private List<Node> open = new List<Node>();
+    private List<Node> closed = new List<Node>();
+
     public List<Vector2Int> FindPathToTarget(Vector2Int startPos, Vector2Int endPos, Cell[,] grid)
     {
+        int HScore = (int)Vector2.Distance(startPos, endPos);
+        Node startNode = new Node(startPos, null, 0, HScore);
+        open.Add(startNode);
+        while(open != null)
+        {
+            Node currentNode = null;
+            foreach(Node n in open)
+            {
+                if (currentNode == null || n.FScore < currentNode.FScore)
+                {
+                    currentNode = n;
+                } else
+                {
+                    continue;
+                }
+                if(currentNode.position == endPos)
+                {
+                    //found end
+                }
+            }
+            GenerateNodeSuccessors(currentNode, startPos, endPos);
+            
+        }
+        //codes in list??
+
         return null;
+    }
+
+    void GenerateNodeSuccessors(Node currentNode, Vector2Int startPos, Vector2Int endPos)
+    {
+        List<Vector2Int> neighbours = new List<Vector2Int>();
+
+        Vector2Int successorPos = currentNode.position + new Vector2Int(-1, 1);
+        Vector2Int successorPos1 = currentNode.position + new Vector2Int(0, 1);
+        Vector2Int successorPos2 = currentNode.position + new Vector2Int(1, 1);
+        Vector2Int successorPos3 = currentNode.position + new Vector2Int(-1, 0);
+        Vector2Int successorPos4 = currentNode.position + new Vector2Int(1, 0);
+        Vector2Int successorPos5 = currentNode.position + new Vector2Int(-1, -1);
+        Vector2Int successorPos6 = currentNode.position + new Vector2Int(0, -1);
+        Vector2Int successorPos7 = currentNode.position + new Vector2Int(1, -1);
+
+        neighbours.Add(successorPos);
+        neighbours.Add(successorPos1);
+        neighbours.Add(successorPos2);
+        neighbours.Add(successorPos3);
+        neighbours.Add(successorPos4);
+        neighbours.Add(successorPos5);
+        neighbours.Add(successorPos6);
+        neighbours.Add(successorPos7);
+
+        for (int i = 0; i < neighbours.Count; i++)
+        {
+            if (true/*no wall here, also no node*/)
+            {
+                int HScore = (int)Vector2.Distance(successorPos, endPos);
+                int GScore = (int)Vector2.Distance(startPos, successorPos);
+                Node successorNode = new Node(startPos, null, GScore, HScore);
+            }
+        }
+        //add them to a list??
+
     }
 
     /// <summary>
